afb4291a01a73893307bc75a73de9206e61574a7
REC-1104 <dhruvparmar110404@gmail.com>
2023-04-09 19:40:12 +0530

    initial commit

---
+++ globe.js
@@ -0,0 +1,11 @@
+VANTA.NET({
+    el: "#container",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.0,
+    minWidth: 200.0,
+    scale: 1.0,
+    scaleMobile: 1.0,
+});
+
